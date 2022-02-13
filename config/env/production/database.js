4c611bf71eb80b63719caf5456b7221d2c7dd4c6
aurudin <renon@renon.com.br>
2022-02-13 13:24:27 -0300

    test: env database url

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -16,5 +16,3 @@ module.exports = ({ env }) => ({
     debug: false,
   },
 });
-
-//postgres://wpyxcrrronhswh:1029f14b4bb98b052e80891c2f0e561de198d703e3f3d3a5e0ca0ca7065d7e1d@ec2-34-233-157-189.compute-1.amazonaws.com:5432/damjint9rlr6s5
